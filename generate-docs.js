15fd1f809731fa86c5a5fce577828fc9f517627c
Rich <rich.fb.email@gmail.com>
2018-07-09 18:10:46 -0500

    Corrected github repository

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v3.0.0`);
+docket.title(`EZ Objects v3.0.1`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
