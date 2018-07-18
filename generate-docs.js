55484977cf5f33b638546c2aa112b15ca7db7c34
Rich <rich.fb.email@gmail.com>
2018-07-17 20:04:39 -0500

    Fixes to last README edits that were in wrong place.

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v4.0.2`);
+docket.title(`EZ Objects v4.0.3`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
