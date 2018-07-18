8fc450421c2d6b606a7ae2eabe98cde7c7cfa295
Rich <rich.fb.email@gmail.com>
2018-07-18 17:23:22 -0500

    Corrected version in README and docs

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v4.1.3`);
+docket.title(`EZ Objects v4.1.0`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
