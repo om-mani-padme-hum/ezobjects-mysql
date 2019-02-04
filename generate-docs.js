36bae4b755586902643785d636e2d3dac38a67f8
Rich <rich.fb.email@gmail.com>
2019-02-03 21:00:57 -0600

    Incrementing version after merge

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v6.1.9`);
+docket.title(`EZ Objects v6.1.10`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
