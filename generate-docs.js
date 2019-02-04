b4f266b3cfdbc99cae4e0d1783f86e9afb967a6d
Rich <rich.fb.email@gmail.com>
2019-02-03 20:59:16 -0600

    Corrected time zone issue with dates

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v6.1.8`);
+docket.title(`EZ Objects v6.1.9`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
