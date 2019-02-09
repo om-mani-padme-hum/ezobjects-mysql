773a897baf97dfcc9244241bbf35db49e5c1b42c
Rich <rich.fb.email@gmail.com>
2019-02-09 17:04:16 -0600

    Corrected behavior for null date values

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v6.1.11`);
+docket.title(`EZ Objects v6.1.12`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`, `mysql-connection.js`]);
 docket.generateDocs(`docs`);
