8c30a30aa0273b3e4b6afd8ee850cc79129e7e21
Rich Lowe <rich.fb.email@gmail.com>
2020-03-14 10:14:59 -0500

    Updated acorn in docket-parser for vuln

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v10.1.9`);
+docket.title(`EZ Objects v10.1.10`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
