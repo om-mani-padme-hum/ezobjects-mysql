6bfa889e7386bdc0af2d6b240d14efd6a6d458f5
Rich Lowe <rich.fb.email@gmail.com>
2020-05-20 12:02:32 -0500

    Bumped docket-parser

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v10.2.0`);
+docket.title(`EZ Objects v10.2.1`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
