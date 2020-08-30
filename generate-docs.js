c84704974a5c8abfa6f4c819bec9eb0b67deddd2
Rich Lowe <rich.fb.email@gmail.com>
2020-08-30 13:58:30 -0500

    Added a second fix for null array properties

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v11.0.2`);
+docket.title(`EZ Objects v11.0.3`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
