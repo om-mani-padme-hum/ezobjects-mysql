ce226eea22e8e354443a974573c439941b80fb9d
Rich Lowe <rich.fb.email@gmail.com>
2021-06-14 14:52:21 -0500

    Fixing version number on documentation

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v12.1.0`);
+docket.title(`EZ Objects v11.2.1`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
