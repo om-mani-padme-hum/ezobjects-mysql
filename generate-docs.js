a9773e62fcfadd5857adbce4e87428bb2cea0810
Rich Lowe <rich.fb.email@gmail.com>
2022-04-11 18:53:55 -0500

    Bumped packages due to vulns

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v11.2.2`);
+docket.title(`EZ Objects v11.2.3`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
