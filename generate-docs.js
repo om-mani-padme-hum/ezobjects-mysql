c1a32bcf8ee29a70608b9e70e49d40ce05e1fc5e
Rich Lowe <rich.fb.email@gmail.com>
2022-04-27 16:31:05 -0500

    Incremented patch version

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v11.2.3`);
+docket.title(`EZ Objects v11.2.4`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
