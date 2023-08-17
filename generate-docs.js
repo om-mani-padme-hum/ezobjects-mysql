3e048ec924c50efb5c234c2fb6969b5b97e29852
Rich Lowe <rich.fb.email@gmail.com>
2023-08-17 12:50:38 -0500

    updated docs and revision to 12.0.0

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v11.7.2`);
+docket.title(`EZ Objects v12.0.0`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
