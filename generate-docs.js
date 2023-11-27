69a398b2e0e46fb14f3f8ff5edb54f29fedfa322
Rich Lowe <rich.fb.email@gmail.com>
2023-11-27 17:39:54 -0600

    Added additional DBM assignInput/assignArrayInputs, and validateInput properties to EZ object type configs

--- generate-docs.js
+++ generate-docs.js
@@ -1,6 +1,6 @@
 const docket = require(`docket-parser`);
 
-docket.title(`EZ Objects v13.0.0`);
+docket.title(`EZ Objects v13.0.1`);
 docket.linkClass(`text-success`);
 docket.parseFiles([`index.js`]);
 docket.generateDocs(`docs`);
